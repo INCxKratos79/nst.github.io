c3a358ef8fcf391427df84c098e8945d721644df
Rishabh Pandey <rishabhp579@gmail.com>
2026-04-25 21:17:19 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2">
+</script>
+<script>
+  const SUPABASE_URL = "https://jlmkgqnxegrtjhbibfbi.supabase.co";
+  const SUPABASE_KEY = "sb_publishable_-XHluHuVjDEkRVhgJJxLUQ_ccwuMfJt";
+
+  const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+</script>
